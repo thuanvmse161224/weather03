424165b2a2cecb09e49c499a8e1e0f77af356099
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-02-14 13:25:39 +0700

    Scaffold basic layout for Homepage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,18 +6,39 @@ import Paper from '@mui/material/Paper';
 
 import useDocumentTitle from '../helpers/useDocumentTitle';
 
+import '../css/weather-icons.min.css';
+
 function Home() {
-    const title = useDocumentTitle('Homepage - Weather Watcher');
+    useDocumentTitle('Homepage - Weather Watcher');
     return (
         <>
-            <Container sx={{m : "15px"}}>
-                <Grid container spacing={2}>
-                    <Grid item sm={8}>
-                    <p>You are in Homepage</p>
-                    <p>Maybe show some info of weather at latest location here ?</p>
+            <Container sx={{mt:"30px",mb:"30px"}}>
+                <Grid container spacing={2} sx={{display:"flex"}}>
+                    <Grid item sm={8} >
+                        <Box sx={{backgroundColor : "#ff232323", p: "15px"}}>
+                            <p>You are in Homepage</p>
+                            <p>Maybe show some info of weather at latest location here ?</p>
+                            <Grid container spacing={2} sx={{display:"flex"}}>
+                                <Grid item sm={12} sx={{display:"flex"}}>
+                                    <Grid item sm={3}>cloud</Grid>
+                                    <Grid item sm={3}>temp</Grid>
+                                    <Grid item sm={6}>city name</Grid>
+                                </Grid>
+                                <Grid item sm={12} sx={{display:"flex"}}>
+                                    <Grid item sm={3}>next day 1
+                                        <i class="wi wi-night-sleet"></i>
+                                    </Grid>
+                                    <Grid item sm={3}>next day 2</Grid>
+                                    <Grid item sm={3}>next day 3</Grid>
+                                    <Grid item sm={3}>next day 4</Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
                     </Grid>
                     <Grid item sm={4}>
-                        <p>Right section</p>
+                        <Box sx={{backgroundColor : "#ff232323", p: "15px"}}>
+                            <p>Right section</p>
+                        </Box>
                     </Grid>
 
                 </Grid>
--- src/components/SearchPageComponent.js
+++ src/components/SearchPageComponent.js
@@ -2,7 +2,7 @@ import { Container, Button, FormGroup, TextField } from "@mui/material";
 import useDocumentTitle from '../helpers/useDocumentTitle';
 
 function SearchPage() {
-    const title = useDocumentTitle('Search Cities - Weather Watcher');
+    useDocumentTitle('Search Cities - Weather Watcher');
     return (
         <>
             <Container sx={{m : "15px"}}>

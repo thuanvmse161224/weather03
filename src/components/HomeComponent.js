2af57ad316c1c844178a84180f62f0c03ad384ef
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-02-15 12:31:09 +0700

    fake data to homepage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,4 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import SearchPage from "./SearchPageComponent";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -15,29 +15,58 @@ function Home() {
             <Container sx={{mt:"30px",mb:"30px"}}>
                 <Grid container spacing={2} sx={{display:"flex"}}>
                     <Grid item sm={8} >
-                        <Box sx={{backgroundColor : "#ff232323", p: "15px"}}>
+                        <Box sx={{backgroundColor : "#ff232323", padding: "15px"}}>
                             <p>You are in Homepage</p>
                             <p>Maybe show some info of weather at latest location here ?</p>
-                            <Grid container spacing={2} sx={{display:"flex"}}>
+                            <Grid container spacing={2} sx={{display:"flex", p: "15px!important", fontFamily: "Public Sans, Helvetica"}}>
                                 <Grid item sm={12} sx={{display:"flex"}}>
-                                    <Grid item sm={3}>cloud</Grid>
-                                    <Grid item sm={3}>temp</Grid>
-                                    <Grid item sm={6}>city name</Grid>
-                                </Grid>
-                                <Grid item sm={12} sx={{display:"flex"}}>
-                                    <Grid item sm={3}>next day 1
-                                        <i class="wi wi-night-sleet"></i>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-day-sunny" style={{fontSize:"100px"}}></i>
                                     </Grid>
-                                    <Grid item sm={3}>next day 2</Grid>
-                                    <Grid item sm={3}>next day 3</Grid>
-                                    <Grid item sm={3}>next day 4</Grid>
+                                    <Grid item sm={3} fontSize="35px">
+                                        23 °C <br/> 
+                                        22:00 PM <br/>
+                                        Sun 12/2
+                                    </Grid>
+                                    <Grid item sm={6} style={{fontSize:"33px"}}>Bình Thạnh district <br/> Hồ Chí Minh City</Grid>
+                                </Grid>
+                                <Grid item columns={15} sm={12} sx={{display:"flex", mt:"30px"}}>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-night-sleet" style={{fontSize:"33px",marginBottom:"20px"}}></i><br/>
+                                        Mon 13/2<br/><br/>
+                                        32 °C - 23 °C
+                                    </Grid>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-night-sleet" style={{fontSize:"33px",marginBottom:"20px"}}></i><br/>
+                                        Tue 14/2<br/><br/>
+                                        32 °C - 23 °C
+                                    </Grid>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-night-sleet" style={{fontSize:"33px",marginBottom:"20px"}}></i><br/>
+                                        Wed 15/2<br/><br/>
+                                        32 °C - 23 °C
+                                    </Grid>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-night-sleet" style={{fontSize:"33px",marginBottom:"20px"}}></i><br/>
+                                        Thu 16/2<br/><br/>    
+                                        32 °C - 23 °C
+                                    </Grid>
+                                    <Grid item sm={3}>
+                                        <i class="wi wi-night-sleet" style={{fontSize:"33px",marginBottom:"20px"}}></i><br/>
+                                        Fri 17/2<br/><br/>    
+                                        32 °C - 23 °C
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Box>
                     </Grid>
                     <Grid item sm={4}>
                         <Box sx={{backgroundColor : "#ff232323", p: "15px"}}>
-                            <p>Right section</p>
+                            <Typography fontSize="40px">
+                                Weather Watcher
+                            </Typography>
+                            <br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit
                         </Box>
                     </Grid>
 
